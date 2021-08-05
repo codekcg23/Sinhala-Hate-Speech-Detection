184281a06e5be7088a6c09a1eba7ce5de735fa2b
codekcg23 <kavigamage23@gmail.com>
2021-08-05 13:01:48 +0530

    Extend dataset combination and apply with TFIDF

--- utills.py
+++ utills.py
@@ -7,7 +7,7 @@ def removePunctuation(txt):
     """
     # set(",.:;'\"-/´`%")
     exclude = set(
-        ".…,‌ ¸‚\"/|—¦”‘\'“’´!?<>#$%^=&*+\-£˜()\[\]{\}:;–Ê_�‪‬‏")
+        ".…,‌ ¸‚\"/|—¦”‘\'“’´!?<>#$%^=&*+\-£˜()\[\]{\}:;–Ê_�‪‬‏\u200b")
 
     return ''.join([(c if c not in exclude else " ") for c in txt])
 
@@ -162,3 +162,65 @@ def PlotRocAuc(y_test, y_pred, color, model_name):
                       yaxis_title="True Positive Rate")
 
     fig.show()
+
+
+def result(y_test, y_pred):
+    import sklearn.metrics as metrics
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import numpy as np
+
+    classes = np.unique(y_test)
+    #y_test_array = pd.get_dummies(y_test, drop_first=False).values
+    ## Accuracy, Precision, Recall
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+    auc = metrics.roc_auc_score(y_test, y_pred)
+    precision = metrics.precision_score(y_test, y_pred)
+    recall = metrics.recall_score(y_true, y_pred)
+    f1_score = metrics.f1_score(y_test, y_pred)
+    print("Accuracy:",  round(accuracy, 2))
+    print("Auc:", round(auc, 2))
+    print("Precsion:", round(precision, 2))
+    print("f1_score:", round(f1_score, 2))
+    print("recall:", round(recall, 2))
+    print("Detail:")
+    print(metrics.classification_report(y_test, y_pred))
+
+    # Plot confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_pred)
+    fig, ax = plt.subplots()
+    sns.heatmap(cm, annot=True, fmt='d', ax=ax, cmap=plt.cm.Blues, cbar=False)
+    ax.set(xlabel="Predicted", ylabel="Actual", xticklabels=classes,
+           yticklabels=classes, title="Confusion matrix")
+    plt.yticks(rotation=0)
+
+    fig, ax = plt.subplots(nrows=1, ncols=2)
+    # Plot roc
+    for i in range(len(classes)):
+        fpr, tpr, thresholds = metrics.roc_curve(y_test[:, i], y_pred[:, i])
+        ax[0].plot(fpr, tpr, lw=3,
+                   label='{0} (area={1:0.2f})'.format(
+                       classes[i], metrics.auc(fpr, tpr))
+                   )
+    ax[0].plot([0, 1], [0, 1], color='navy', lw=3, linestyle='--')
+    ax[0].set(xlim=[-0.05, 1.0], ylim=[0.0, 1.05],
+              xlabel='False Positive Rate',
+              ylabel="True Positive Rate (Recall)",
+              title="Receiver operating characteristic")
+    ax[0].legend(loc="lower right")
+    ax[0].grid(True)
+
+    # Plot precision-recall curve
+    for i in range(len(classes)):
+        precision, recall, thresholds = metrics.precision_recall_curve(
+            y_test[:, i], y_pred[:, i])
+        ax[1].plot(recall, precision, lw=3,
+                   label='{0} (area={1:0.2f})'.format(classes[i],
+                                                      metrics.auc(recall, precision))
+                   )
+    ax[1].set(xlim=[0.0, 1.05], ylim=[0.0, 1.05], xlabel='Recall',
+              ylabel="Precision", title="Precision-Recall curve")
+    ax[1].legend(loc="best")
+    ax[1].grid(True)
+    plt.show()
+    return accuracy, f1_score, recall, precision, auc
